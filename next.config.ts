aab087b7a684be30fb063507399f6761a8e9dc38
John St. Aïmond Banson <articwolf177@gmail.com>
2024-10-30 23:29:12 +0100

    completed:landing-page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
 };
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pexels.com',
+        port: '',
+        pathname: '',
+      },
+    ],
+  },
+}
 
 export default nextConfig;
